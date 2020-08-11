ac7f7b5cba764b069230492aebbe2405ecb3a65d
akshayjyothis <64101639+akshayjyothis@users.noreply.github.com>
2020-08-11 23:02:28 +0530

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,14 @@
+var feedback = function(res) {
+    if (res.success === true) {
+        var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
+        document.querySelector('.status').classList.add('bg-success');
+        document.getElementById("url").value= get_link;
+        //document.querySelector('.status').innerHTML =
+           // 'Image : ' + '<br><input class="image-url" value=\"' + get_link + '\"/>' + '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
+    }
+};
+
+new Imgur({
+    clientid: '83ade5ae4714232', //You can change this ClientID
+    callback: feedback
+});
