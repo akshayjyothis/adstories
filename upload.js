2f864472f4394fdc2e62af79a11e2ff57510969f
haridot04 <harikrishnanru04@gmail.com>
2020-08-13 01:41:25 +0530

    metadata added

--- upload.js
+++ upload.js
@@ -2,7 +2,7 @@ var feedback = function(res) {
     if (res.success === true) {
         var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
         document.querySelector('.status').classList.add('bg-success');
-        document.getElementById("wait").innerHTML = "Upload Successfully";
+        document.getElementById("wait").innerHTML = "Uploaded Successfully";
         document.getElementById("wait").style.color = "green";
         document.getElementById("url").value= get_link;
         //document.querySelector('.status').innerHTML =
